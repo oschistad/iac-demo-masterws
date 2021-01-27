4200292215f1f3e6712df065fd96984bee232295
OleAndréSchistad <oas@aztek.no>
2021-01-27 19:53:15 +0100

    Move variables to proper place

--- main.tf
+++ main.tf
@@ -2,12 +2,6 @@ variable "tfe_orgname" {
   default = "aztekdemo"
 }
 
-variable "subscription_id" {}
-variable "client_id" {}
-variable "tenant_id" {}
-variable "client_secret" {}
-variable "tfe_token" {}
-variable "github_oauth_token" {}
 
 locals {
   tfe_vars = {
---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "subscription_id" {}
+variable "client_id" {}
+variable "tenant_id" {}
+variable "client_secret" {}
+variable "tfe_token" {}
+variable "github_oauth_token" {}
